e40dc023934a9022a81984c62b863b6179ecdab1
Lazhari <lazhari.mohammed@outlook.com>
2016-04-20 14:12:30 +0100

    list poll end point

--- server/api/poll/poll.controller.js
+++ server/api/poll/poll.controller.js
@@ -13,90 +13,111 @@ import _ from 'lodash';
 import Poll from './poll.model';
 
 function respondWithResult(res, statusCode) {
-  statusCode = statusCode || 200;
-  return function(entity) {
-    if (entity) {
-      res.status(statusCode).json(entity);
-    }
-  };
+    statusCode = statusCode || 200;
+    return function(entity) {
+        if (entity) {
+            res.status(statusCode).json(entity);
+        }
+    };
 }
 
 function saveUpdates(updates) {
-  return function(entity) {
-    var updated = _.merge(entity, updates);
-    return updated.saveAsync()
-      .spread(updated => {
-        return updated;
-      });
-  };
+    return function(entity) {
+        var updated = _.merge(entity, updates);
+        return updated.saveAsync()
+            .spread(updated => {
+                return updated;
+            });
+    };
 }
 
 function removeEntity(res) {
-  return function(entity) {
-    if (entity) {
-      return entity.removeAsync()
-        .then(() => {
-          res.status(204).end();
-        });
-    }
-  };
+    return function(entity) {
+        if (entity) {
+            return entity.removeAsync()
+                .then(() => {
+                    res.status(204).end();
+                });
+        }
+    };
 }
 
 function handleEntityNotFound(res) {
-  return function(entity) {
-    if (!entity) {
-      res.status(200).end({ok:false, message:'Poll not found!', status: 404});
-      return null;
-    }
-    return entity;
-  };
+    return function(entity) {
+        if (!entity) {
+            res.status(200).end({
+                ok: false,
+                message: 'Poll not found!',
+                status: 404
+            });
+            return null;
+        }
+        return entity;
+    };
 }
 
 function handleError(res, statusCode) {
-  statusCode = statusCode || 500;
-  return function(err) {
-    res.status(200).send({ok:false, message: err.message, status: statusCode, err: err});
-  };
+    statusCode = statusCode || 500;
+    return function(err) {
+        res.status(200).send({
+            ok: false,
+            message: err.message,
+            status: statusCode,
+            err: err
+        });
+    };
 }
 
 // Gets a list of Polls
 export function index(req, res) {
-  Poll.findAsync()
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    let currentUser = req.user._id;
+    let query = {
+        deleted: {
+            $ne: true
+        }
+    };
+    if(req.user.role !== 'admin') {
+        query.$or = [
+            {owner: currentUser},
+            {public: true}
+        ]
+    }
+    Poll.findAsync(query)
+        .then(respondWithResult(res))
+        .catch(handleError(res));
 }
 
 // Gets a single Poll from the DB
 export function show(req, res) {
-  Poll.findByIdAsync(req.params.id)
-    .then(handleEntityNotFound(res))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    Poll.findByIdAsync(req.params.id)
+        .then(handleEntityNotFound(res))
+        .then(respondWithResult(res))
+        .catch(handleError(res));
 }
 
 // Creates a new Poll in the DB
 export function create(req, res) {
-  Poll.createAsync(req.body)
-    .then(respondWithResult(res, 201))
-    .catch(handleError(res));
+    Poll.createAsync(req.body)
+        .then(respondWithResult(res, 201))
+        .catch(handleError(res));
 }
 
 // Updates an existing Poll in the DB
 export function update(req, res) {
-  if (req.body._id) {
-    delete req.body._id;
-  }
-  Poll.findByIdAsync(req.params.id)
-    .then(handleEntityNotFound(res))
-    .then(saveUpdates(req.body))
-    .then(respondWithResult(res))
-    .catch(handleError(res));
+    if (req.body._id) {
+        delete req.body._id;
+    }
+    Poll.findByIdAsync(req.params.id)
+        .then(handleEntityNotFound(res))
+        .then(saveUpdates(req.body))
+        .then(respondWithResult(res))
+        .catch(handleError(res));
 }
 
 // Deletes a Poll from the DB
 export function destroy(req, res) {
-  Poll.findByIdAsync(req.params.id)
-    .then(handleEntityNotFound(res))
-    .then(removeEntity(res))
-    .catch(handleError(res));
+    Poll.findByIdAsync(req.params.id)
+        .then(handleEntityNotFound(res))
+        .then(removeEntity(res))
+        .catch(handleError(res));
 }
--- server/api/poll/poll.model.js
+++ server/api/poll/poll.model.js
@@ -3,25 +3,37 @@
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 
 var PollSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  type: {
-    type: String,
-    enum: ['single', 'multiple', 'satisfaction']
-  },
-  choices: [],
-  active: Boolean,
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  updated: {
-    type: Date
-  },
-  token: {
-      type: String,
-      unique: true
-  }
+    title: String,
+    description: String,
+    type: {
+        type: String,
+        enum: ['single', 'multiple', 'satisfaction']
+    },
+    choices: [],
+    active: Boolean,
+    owner: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+    },
+    public: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date
+    },
+    token: {
+        type: String,
+        unique: true
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    }
 });
 
 export default mongoose.model('Poll', PollSchema);
