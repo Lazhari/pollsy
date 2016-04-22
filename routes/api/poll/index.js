11156f04fdf4885a14bf328b3307739fc1f115e2
Mohamed Atouch <atouch.mohamed@gmail.com>
2016-04-22 11:18:38 +0100

    Remove requests id checks

--- routes/api/poll/index.js
+++ routes/api/poll/index.js
@@ -4,9 +4,10 @@ const router = require('express').Router();
 const validate = require('express-validation');
 const controller = require('./poll.controller.js');
 const pollValidation = require('../../validations/polls.js');
+const auth = require('../../core/auth/auth.service.js');
 
 router
-    .get('/', controller.getAll)
+    .get('/', auth.isAuthenticated(), controller.getAll)
     .get('/:id', controller.get)
     .post('/', validate(pollValidation.create), controller.create)
     .put('/:id', controller.update)
--- routes/api/poll/poll.controller.js
+++ routes/api/poll/poll.controller.js
@@ -1,92 +1,78 @@
-'use strict'
+'use strict';
 const Poll = require('../../../models/poll.js');
 exports.getAll = (req, res, next) => {
     Poll.find({
-        publisher: req.user._id
         deleted: {
             $ne: true
         }
+        ,$or: [{
+            publisher: req.user._id
+        }, {
+            public: true
+        }]
     }, (err, polls) => {
         if (err) {
             return res.send({
                 ok: false,
-                message: "Coudn't get polls"
-            })
+                message: 'Coudn\'t get polls'
+            });
         }
         return res.send(polls);
     });
-}
+};
 exports.get = (req, res, next) => {
-    req.checkParams('id', 'The user id is invalid').isMongoId();
-    let errors = req.validationErrors();
-    if (errors) {
-        return res.send({
-            ok: false,
-            message: errors[0].msg
-        });
-    } else {
-        Poll.findOne({
-                _id: req.params.id,
-                deleted: {
-                    $ne: true
-                }
-            })
-            .select('-deleted')
-            .exec((err, poll) => {
-                if (err) {
-                    return res.send({
-                        ok: false,
-                        message: "Coudn't get the poll"
-                    });
-                }
-
-                if (!poll) {
-                    return res.send({
-                        ok: false,
-                        message: "Coudn't get the poll"
-                    });
-                }
-
-                return res.send(poll);
-            });
-    }
-}
-
-exports.create = (req, res, next) => {
-
-}
-
-
-exports.delete = (req, res, next) => {
-    req.checkParams('id', 'Invalid Id').isMongoId();
-    let errors = req.validationErrors();
-    if (errors) {
-        return res.send({
-            ok: false,
-            message: errors[0].msg
-        });
-    } else {
-        Poll.update({
+    Poll.findOne({
             _id: req.params.id,
-            publisher: req.user._id
-        }, {
-            $set: {
-                deleted: true
+            deleted: {
+                $ne: true
             }
-        }, (err, raw) => {
+        })
+        .select('-deleted')
+        .exec((err, poll) => {
             if (err) {
                 return res.send({
                     ok: false,
-                    message: "Coudn't delete the poll"
+                    message: 'Coudn\'t get the poll'
                 });
             }
-            return res.send({
-                ok: true
-            });
+
+            if (!poll) {
+                return res.send({
+                    ok: false,
+                    message: 'Coudn\'t get the poll'
+                });
+            }
+
+            return res.send(poll);
         });
-    }
-}
+};
+
+exports.create = (req, res, next) => {
+
+};
+
+exports.delete = (req, res, next) => {
+    Poll.update({
+        _id: req.params.id,
+        publisher: req.user._id
+    }, {
+        $set: {
+            updated: new Date(),
+            deleted: true
+        }
+    }, (err, raw) => {
+        if (err) {
+            return res.send({
+                ok: false,
+                message: 'Coudn\'t delete the poll'
+            });
+        }
+        return res.send({
+            ok: true
+        });
+    });
+};
 
 exports.update = (req, res, next) => {
 
-}
+};
