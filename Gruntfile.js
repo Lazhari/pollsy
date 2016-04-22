a9fbd58cb7af26db24330c1d0ce8315903c57665
Lazhari <lazhari.mohammed@outlook.com>
2016-04-22 13:41:40 +0100

    add mocha test task runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,9 +16,20 @@ module.exports = function(grunt) {
                 dest: "./specs-doc/",
                 options: {
                     debug: true,
-                    includeFilters: [ ".*\\.js$" ],
-                    excludeFilters: [ "node_modules/" ]
+                    includeFilters: [".*\\.js$"],
+                    excludeFilters: ["node_modules/"]
                 }
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['routes/**/test/*.js']
             }
         },
         nodemon: {
@@ -36,13 +47,13 @@ module.exports = function(grunt) {
                     watch: ['**'],
                     delay: 1000,
                     legacyWatch: true,
-                    callback: function (nodemon) {
-                        nodemon.on('log', function (event) {
+                    callback: function(nodemon) {
+                        nodemon.on('log', function(event) {
                             console.log(event.colour);
                         });
 
                         // opens browser on initial server start
-                        nodemon.on('config:update', function () {
+                        nodemon.on('config:update', function() {
                             // Delay before server listens on port
                             setTimeout(function() {
                                 require('open')('http://localhost:9090');
@@ -50,7 +61,7 @@ module.exports = function(grunt) {
                         });
 
                         // refreshes browser when server reboots
-                        nodemon.on('restart', function () {
+                        nodemon.on('restart', function() {
                             // Delay before server listens on port
                             setTimeout(function() {
                                 require('fs').writeFileSync('.rebooted', 'rebooted');
@@ -63,8 +74,10 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-apidoc');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-nodemon');
-    grunt.registerTask('doc',['apidoc']);
-    grunt.registerTask('serve',['nodemon']);
-    grunt.registerTask('default', ['doc','nodemon', 'watch']);
+    grunt.registerTask('doc', ['apidoc']);
+    grunt.registerTask('serve', ['nodemon']);
+    grunt.registerTask('test', ['mochaTest']);
+    grunt.registerTask('default', ['doc', 'nodemon', 'mochaTest','watch']);
 };
